6a94d9299e7f175e06a643464ca0cfe7d7a5b257
Moona-al <sw7475924@gmail.com>
2026-06-10 21:13:34 +0700

    navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -148,10 +148,10 @@ export default function Navbar() {
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
                   <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
                 </svg>
-                <span>@AETHER.STUDIOS</span>
+                <span>@peaceminusonedotcom</span>
               </div>
               <span className="text-[10px] font-mono tracking-widest text-muted-foreground uppercase">
-                EST. 2020
+                EST. 2016
               </span>
             </motion.div>
           </motion.div>
